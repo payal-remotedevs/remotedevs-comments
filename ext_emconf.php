aaa748409d2bf365b65fbe7717f7994ac767bb08
Payal Solanki <108181624+payal-remotedevs@users.noreply.github.com>
2026-03-23 10:59:54 +0530

    Fix news extension version constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
-            'news' => '9.1.0-11.99.99'
+            'news' => '9.1.0-11.9.99'
         ],
         'conflicts' => [],
         'suggests' => [],
